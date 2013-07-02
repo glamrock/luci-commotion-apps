430803208e9eb7e3d4ac212e90288f4c03c3c476
Dan Staples <dan.staples@riseup.net>
2013-07-02 11:17:03 -0400

    create visual pattern from fingerprint, instead of a colorbar

--- luci-static/commotion/apps.js
+++ luci-static/commotion/apps.js
@@ -8,3 +8,11 @@ function ReverseDisplay(d) {
 if(document.getElementById(d).style.display == "none") { document.getElementById(d).style.display = "block"; }
 else { document.getElementById(d).style.display = "none"; }
 }
+Zepto(function($){
+  $('.app-canvas').each(function(el) {
+    if($(this).data('tip')) {
+      $(this).parent().after('<p class="tip">' + $(this).data('tip') + '</p>')
+    }
+  });
+  $('.app-tippable').on('click', function(ev) { $(this).parent().next('p').toggle() });
+});
